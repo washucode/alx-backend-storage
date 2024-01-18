0d21e5381b0bbf389fcf8625acff73b4c3466012
washucode <estherw8525@gmail.com>
2024-01-19 02:35:10 +0300

    task 2 fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,5 +2,5 @@
 -- and the number of non unique fans they have
 
 
-SELECT origin, COUNT(DISTINCT(fans)) AS nb_fans FROM metal_bands
+SELECT origin, COUNT(fans) AS nb_fans FROM metal_bands
 GROUP BY origin ORDER BY nb_fans DESC;
