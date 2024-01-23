0dc94054d87daff42907c2bae2ac14ca21bceec7
washucode <estherw8525@gmail.com>
2024-01-23 12:38:06 +0300

    task 14

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 101. Top students """
+
+import pymongo
+
+
+def top_students(mongo_collection):
+    """ returns all students sorted by average score """
+    return mongo_collection.aggregate([
+        {"$project": {"name": "$name", "averageScore": {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ])
