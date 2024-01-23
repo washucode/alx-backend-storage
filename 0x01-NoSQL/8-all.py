bf50e513dc671f99829fd45a8ee735efe6cbc864
washucode <estherw8525@gmail.com>
2024-01-23 11:18:39 +0300

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 8. List all documents in Python """
+
+
+
+def list_all(mongo_collection):
+    """ lists all documents in a collection """
+    return mongo_collection.find()
