91634ce2b6df4431294b5e2bc52a2dc7270a8d78
washucode <estherw8525@gmail.com>
2024-01-26 22:34:43 +0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,3 +26,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str) -> bytes:
+        """
+        Method that gets the data from Redis
+        """
+        data = self._redis.get(key)
+        return data
+    
+    def get_str(self, key: str) -> str:
+        """
+        Method that gets the data from Redis
+        """
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+    
+    def get_int(self, key: str) -> int:
+        """
+        Method that gets the data from Redis
+        """
+        data = self._redis.get(key)
+        return int(data)
