f6538c0c126ae757a1966bbb04a13911785e4cd9
washucode <estherw8525@gmail.com>
2024-01-26 22:35:25 +0300

    fix pycidestyle errors

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,21 +26,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
     def get(self, key: str) -> bytes:
         """
         Method that gets the data from Redis
         """
         data = self._redis.get(key)
         return data
-    
+
     def get_str(self, key: str) -> str:
         """
         Method that gets the data from Redis
         """
         data = self._redis.get(key)
         return data.decode("utf-8")
-    
+
     def get_int(self, key: str) -> int:
         """
         Method that gets the data from Redis
