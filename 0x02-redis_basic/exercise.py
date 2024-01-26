53bbe5bc03409d29a927136f054e68bbe54d5633
washucode <estherw8525@gmail.com>
2024-01-26 22:31:25 +0300

    project setup + task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+Redis basic exercise
+"""
+
+import redis
+import uuid
+
+class Cache:
+    """
+    Cache class
+    """
+    def __init__(self):
+        """
+        Constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes) -> str:
+        """
+        Method that generates a random key and stores the input data in Redis
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+    
+    
